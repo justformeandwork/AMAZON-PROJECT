ab4226ac8706f2b972d60f175b4bdf4fb81c7c27
justformeandwork <justformeandwork@gmail.com>
2025-02-18 20:06:05 +0530

    adding more than 1 product to cart at a time

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product)=>{
             </div>
 
             <div class="product-quantity-container">
-              <select>
+              <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -56,6 +56,7 @@ document.querySelector('.js-products-grid').innerHTML=productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId=button.dataset.productId;
+    let quantity=Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     let matchingItem;
     cart.forEach((item)=>{
       if(productId===item.productId){
@@ -63,12 +64,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       }
     });
     if(matchingItem){
-      matchingItem.quantity++;
+      matchingItem.quantity=quantity;
     }
     else{
     cart.push({
       productId:productId,
-      quantity:1
+      quantity:quantity
     });
     }
     let cartQuantity=0;
