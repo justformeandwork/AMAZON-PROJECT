4df4e7873f1faa629144a27c6536ee023a43f595
justformeandwork <justformeandwork@gmail.com>
2025-02-21 22:25:14 +0530

    move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,19 @@
 export let cart=[];
+export function addToCart(productId){
+  let matchingItem;
+  let quantity=Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    cart.forEach((cartItem)=>{
+      if(productId===cartItem.productId){
+        matchingItem=cartItem;
+      }
+    });
+    if(matchingItem){
+      matchingItem.quantity+=quantity;
+    }
+    else{
+    cart.push({
+      productId, 
+      quantity
+    });
+    }
+}
