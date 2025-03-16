3666761aa6e96d2e021a29de2e75bed1d49a8838
justformeandwork <justformeandwork@gmail.com>
2025-03-16 17:05:35 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products,getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 export function renderOrderSummary(){
   let cartSummaryHTML='';
   cart.forEach((cartItem)=>{
@@ -91,6 +92,7 @@ export function renderOrderSummary(){
       let productId=link.dataset.productId;    
       removeFromCart(productId);
       document.querySelector(`.js-cart-item-container-${productId}`).remove();
+      renderPaymentSummary();
     });
   });
   displayQuantity();
@@ -120,6 +122,7 @@ export function renderOrderSummary(){
       const deliveryOptionId=element.dataset.deliveryOptionId;
       updateDeliveryOption(productId,deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
