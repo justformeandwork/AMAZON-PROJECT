52dedd874f5a130ebdd7ad6da28013531168d7bf
justformeandwork <justformeandwork@gmail.com>
2025-04-11 23:02:34 +0530

    remove default test and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,12 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+describe('test suite:format currency',()=>{
+  it('Converts cents into dollars',()=>{
+    expect(formatCurrency(2045)).toEqual('20.45');
+  });
+  it('works with 0',()=>{
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+  it('rounds up to the nearst cent',()=>{
+    expect(formatCurrency(2045.5)).toEqual('20.46');
+  });
+});
