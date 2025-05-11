1b24dc9deb35b8bf4ef1284cbe933b527d91d2e3
justformeandwork <justformeandwork@gmail.com>
2025-05-11 16:54:59 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,20 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js';
 //import '../data/BACKEND-PRATICE.js';
 //import '../data/car.js';
+async function loadPage(){
+  await loadProductsFetch();
+  await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve();
+    });
+  });
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage().then(()=>{
+  console.log('page loaded');
+});
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve)=>{
@@ -16,6 +30,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 /*
 new Promise((resolve)=>{
   loadProducts(()=>{
